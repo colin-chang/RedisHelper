0b7ce02d0f4a7455f3d94d6264a3ba929e557570
Colin Chang <zhangcheng5468@gmail.com>
2020-08-21 01:08:17 +0800

    修正配置校验

--- ColinChang.RedisHelper/RedisHelperOptions.cs
+++ ColinChang.RedisHelper/RedisHelperOptions.cs
@@ -4,9 +4,10 @@ namespace ColinChang.RedisHelper
 {
     public class RedisHelperOptions
     {
+        [Required(ErrorMessage = "redis connection string is required")]
         public string ConnectionString { get; set; }
 
-        [Range(0, 15, ErrorMessage = "DbNumber must be between 0 and 15")]
+        [Range(0, 15, ErrorMessage = "redis db number must be between 0 and 15")]
         public int DbNumber { get; set; }
     }
 }
