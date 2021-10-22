164e5dee9f5976575448e4d80ba48b17f48b0473
songzheng45 <songzheng@gmail.com>
2021-10-22 12:07:57 +0800

    将模块封装成一个 ABP 模块, 方便ABP项目中引用

---
+++ ColinChang.RedisHelper.Abp/RedisHelperModule.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Modularity;
+
+namespace ColinChang.RedisHelper.Abp
+{
+    public class RedisHelperModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            var services = context.Services;
+            var configuration = context.Services.GetConfiguration();
+
+            services.AddOptions<RedisHelperOptions>()
+              .Configure(configuration.Bind)
+              .ValidateDataAnnotations();
+            services.AddSingleton<IRedisHelper, RedisHelper>();
+        }
+    }
+}
